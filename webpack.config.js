52f5e7a27bdbc9adcc1c83404a8493caff10a25e
dcharney-work <delaneycharney@gmail.com>
2021-03-11 07:39:46 +0100

    finished image optimization

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,29 @@ module.exports = {
         path: __dirname + "/dist",
         filename: '[name].bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name (filename) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../","/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
